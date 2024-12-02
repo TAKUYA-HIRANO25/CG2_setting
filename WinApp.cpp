ba664ba54d2972c598e7df6f064098615b7b8c36
TAKUYA-HIRANO25 <k023g0013@g.neec.ac.jp>
2024-12-02 14:12:45 +0900

    修正

--- WinApp.cpp
+++ WinApp.cpp
@@ -1,5 +1,14 @@
 #include "WinApp.h"
-#include <cassert>
+#define DIRECTINPUT_VERSION 0x0800
+#include "externals/DirectXTex/DirectXTex.h"
+#include "externals/imgui/imgui.h"
+#include "externals/imgui/imgui_impl_dx12.h"
+#include "externals/imgui/imgui_impl_win32.h"
+#include "externals/DirectXTex/d3dx12.h"
+#pragma comment(lib,"dinput8.lib")
+#pragma comment(lib,"dxguid.lib")
+
+extern IMGUI_IMPL_API LRESULT ImGui_ImplWin32_WndProcHandler(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
 
 
 LRESULT CALLBACK WinApp::WindowProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam)
--- WinApp.h
+++ WinApp.h
@@ -1,19 +1,8 @@
 #pragma once
 #include "Windows.h"
-#define DIRECTINPUT_VERSION 0x0800
 #include <dinput.h>
 #include <wrl.h>
 #include <cstdint>
-
-#pragma comment(lib,"dinput8.lib")
-#pragma comment(lib,"dxguid.lib")
-#include "externals/DirectXTex/DirectXTex.h"
-#include "externals/imgui/imgui.h"
-#include "externals/imgui/imgui_impl_dx12.h"
-#include "externals/imgui/imgui_impl_win32.h"
-#include "externals/DirectXTex/d3dx12.h"
-
-extern IMGUI_IMPL_API LRESULT ImGui_ImplWin32_WndProcHandler(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
 
 class WinApp {
 public:
