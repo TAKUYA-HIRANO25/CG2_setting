f7a18159580c6436fa14e51bb53ed71f933b94d0
TAKUYA-HIRANO25 <k023g0013@g.neec.ac.jp>
2024-12-16 14:59:43 +0900

    修正

--- main.cpp
+++ main.cpp
@@ -1503,13 +1503,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	//debugController->Release();
 #endif _DEBUG
 	//警告
-	/*IDXGIDebug1* debug;
-	if (SUCCEEDED(DXGIGetDebugInterface1(0, IID_PPV_ARGS(&debug)))) {
-		debug->ReportLiveObjects(DXGI_DEBUG_ALL, DXGI_DEBUG_RLO_ALL);
-		debug->ReportLiveObjects(DXGI_DEBUG_APP, DXGI_DEBUG_RLO_ALL);
-		debug->ReportLiveObjects(DXGI_DEBUG_D3D12, DXGI_DEBUG_RLO_ALL);
-		debug->Release();
-	}*/
+
 	winApp->Finalize();
 	delete winApp;
 	return 0;
