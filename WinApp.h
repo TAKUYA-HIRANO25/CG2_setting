27250093194d07195d2e64d2b30dcc9a700485e0
TAKUYA-HIRANO25 <k023g0013@g.neec.ac.jp>
2024-12-16 15:04:43 +0900

    修正

--- WinApp.h
+++ WinApp.h
@@ -8,12 +8,7 @@
 #pragma comment(lib,"dinput8.lib")
 #pragma comment(lib,"dxguid.lib")
 #include "externals/DirectXTex/DirectXTex.h"
-#include "externals/imgui/imgui.h"
-#include "externals/imgui/imgui_impl_dx12.h"
-#include "externals/imgui/imgui_impl_win32.h"
 #include "externals/DirectXTex/d3dx12.h"
-
-extern IMGUI_IMPL_API LRESULT ImGui_ImplWin32_WndProcHandler(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
 
 class WinApp {
 public:
